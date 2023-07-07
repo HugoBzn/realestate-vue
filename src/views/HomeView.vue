f5ab933efb1595450c40a798dac8c671befebf88
HugoBzn <hugobazan1499@gmail.com>
2023-07-07 14:24:12 -0600

    Vuetify installed and configured

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,8 +1,7 @@
-<script setup>
-</script>
+<script setup></script>
 
 <template>
   <main>
-    <h1>Vue Bienes Raices</h1>
+    <h1 class="text-center text-h3 font-weight-bold my-5">Vue Bienes Raices</h1>
   </main>
 </template>
