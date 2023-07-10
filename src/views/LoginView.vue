c7299b6ef39d07d42b0cecbb947341afdf234d43
HugoBzn <hugobazan1499@gmail.com>
2023-07-10 13:30:04 -0600

    Form ready with validation

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,4 +1,16 @@
-<script setup></script>
+<script setup>
+import { useForm, useField } from 'vee-validate';
+import { loginSchema as validationSchema } from '../validation/loginSchema';
+
+const { handleSubmit } = useForm({ validationSchema });
+
+const email = useField('email');
+const password = useField('password');
+
+const submit = handleSubmit(() => {
+  console.log('Subiendo...');
+});
+</script>
 
 <template>
   <v-card flat max-width="600" class="mx-auto">
@@ -6,9 +18,23 @@
     <v-card-subtitle class="text-h5">Inicia Sesión con tu Cuenta</v-card-subtitle>
 
     <v-form class="mt-5">
-      <v-text-field type="email" label="Email" bg-color="blue-grey-lighten-5" />
-      <v-text-field type="password" label="Password" bg-color="blue-grey-lighten-5" />
-      <v-btn block color="pink-accent-3">Inciar sesión</v-btn>
+      <v-text-field
+        type="email"
+        label="Email"
+        bg-color="blue-grey-lighten-5"
+        class="mb-3"
+        v-model="email.value.value"
+        :error-messages="email.errorMessage.value"
+      />
+      <v-text-field
+        type="password"
+        label="Password"
+        bg-color="blue-grey-lighten-5"
+        class="mb-3"
+        v-model="password.value.value"
+        :error-messages="password.errorMessage.value"
+      />
+      <v-btn block color="pink-accent-3" @click="submit">Inciar sesión</v-btn>
     </v-form>
   </v-card>
 </template>
