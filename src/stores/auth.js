c5a5c2b60836325b4c816edc272749334fe7c1e1
HugoBzn <hugobazan1499@gmail.com>
2023-07-11 13:22:45 -0600

    Get data if user is already authenticated

--- src/stores/auth.js
+++ src/stores/auth.js
@@ -1,18 +1,26 @@
-import { ref, computed } from 'vue';
+import { onMounted, ref, computed } from 'vue';
 import { defineStore } from 'pinia';
 import { useFirebaseAuth } from 'vuefire';
-import { signInWithEmailAndPassword } from 'firebase/auth';
+import { signInWithEmailAndPassword, onAuthStateChanged } from 'firebase/auth';
 
 export const useAuthStore = defineStore('auth', () => {
   const auth = useFirebaseAuth();
 
   const errorMsg = ref('');
-  const authUser = ref({});
+  const authUser = ref(null);
 
   const errorCodes = {
     'auth/user-not-found': 'Usuario no encontrado',
     'auth/wrong-password': 'El password es incorrecto'
   };
+
+  onMounted(() => {
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        authUser.value = user;
+      }
+    });
+  });
 
   const login = ({ email, password }) => {
     signInWithEmailAndPassword(auth, email, password)
