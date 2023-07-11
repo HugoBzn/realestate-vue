d63e58dd0385d5b329aa3fa5ee0a33c3d22d52f9
HugoBzn <hugobazan1499@gmail.com>
2023-07-10 20:53:58 -0600

    Install firebase and configured, ready to work

--- src/main.js
+++ src/main.js
@@ -1,28 +1,38 @@
-import './assets/main.css'
+import './assets/main.css';
 
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
+import { createApp } from 'vue';
+import { createPinia } from 'pinia';
 
 // Vuetify
-import 'vuetify/styles'
-import { createVuetify } from 'vuetify'
-import * as components from 'vuetify/components'
-import * as directives from 'vuetify/directives'
+import 'vuetify/styles';
+import { createVuetify } from 'vuetify';
+import * as components from 'vuetify/components';
+import * as directives from 'vuetify/directives';
 
-import App from './App.vue'
-import router from './router'
+// Firebase
+import { VueFire, VueFireAuth } from 'vuefire';
+import { firebaseApp } from './config/firebase';
 
-const app = createApp(App)
+import App from './App.vue';
+import router from './router';
+
+const app = createApp(App);
 
 // Vuetify
 const vuetify = createVuetify({
   components,
   directives
-})
+});
 
-app.use(vuetify)
+// Firebase
+app.use(VueFire, {
+  firebaseApp,
+  modules: [VueFireAuth]
+});
 
-app.use(createPinia())
-app.use(router)
+app.use(vuetify);
 
-app.mount('#app')
+app.use(createPinia());
+app.use(router);
+
+app.mount('#app');
