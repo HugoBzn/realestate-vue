1731c5bab2d1f7b4259345be0b5be830fb079cc8
HugoBzn <hugobazan1499@gmail.com>
2023-07-17 20:05:24 -0600

    Properties list in home Add styles to home We can see the properties

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,21 @@
-<script setup></script>
+<script setup>
+import Propiedad from '../components/Propiedad.vue';
+import usePropiedades from '../composables/usePropiedades';
+
+const { propiedadesCollection, propertyPrice } = usePropiedades();
+</script>
 
 <template>
-  <h1 class="text-center text-h3 font-weight-bold my-5">Vue Bienes Raices</h1>
+  <h1 class="text-center text-h3 font-weight-bold my-5">Nuestras propiedades</h1>
+
+  <vcard flat class="py-10">
+    <v-row>
+      <Propiedad
+        v-for="propiedad in propiedadesCollection"
+        :key="propiedad.id"
+        :propiedad="propiedad"
+        :price="propertyPrice"
+      />
+    </v-row>
+  </vcard>
 </template>
